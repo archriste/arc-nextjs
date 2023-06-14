65612d662fdf73766d765120bcd7cfbfc37e156f
Alain Christe <alain-christetello@uiowa.edu>
2023-06-13 22:36:01 -0500

    added build settings

--- next.config.js
+++ next.config.js
@@ -1,9 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-        images: true,
-    },
-    output: 'export',
+// next.config.js
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
 }
 
-module.exports = nextConfig
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    loader: 'imgix',
+    path: 'archriste.imgix.net',
+  },
+}

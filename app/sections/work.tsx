6e90d94ef57829ba57129366173e9d8c11204074
Alain Christe <alain-christetello@uiowa.edu>
2023-07-05 15:28:30 -0500

    Fixed typo

--- app/sections/work.tsx
+++ app/sections/work.tsx
@@ -6,7 +6,7 @@ export default function Work() {
         <Anchor id="work" />
         <div className="">
             <h1 className="font-inter text-center pb-8 text-5xl font-bold text-white drop-shadow-xl sm:text-left">Work</h1>
-            <h2 className="font-inter px-10 text-2xl text-white drop-shadow-xl sm:px-0">This section is for work I've undertaken professionally.</h2>
+            <h2 className="font-inter px-10 text-2xl text-white drop-shadow-xl sm:px-0">This section is for work I&apos;ve undertaken professionally.</h2>
         </div>
     </section>
   );
